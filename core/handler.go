f936b732e40f7fabc92904cd70f906d82f893ee2
Dimonchik0036 <dimonchik0036@gmail.com>
2017-08-05 01:23:49 +0700

    Add command handler

--- core/handler.go
+++ core/handler.go
@@ -1,1 +1,19 @@
 package core
+
+type Handler struct {
+	PermissionLevel int
+	Handler         func(user *User, command Command)
+}
+
+func CommandHandler(user *User, command Command, handlers map[string]Handler) {
+	handler, ok := handlers[command.Command]
+	if !ok || !checkHandler(user, handler) {
+		return
+	}
+
+	handler.Handler(user, command)
+}
+
+func checkHandler(user *User, handler Handler) bool {
+	return user.Permission >= handler.PermissionLevel
+}
