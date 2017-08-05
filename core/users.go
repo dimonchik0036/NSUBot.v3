e231c5fc35f7bc73e58168d7d9659e0e5cd1cf15
Dimonchik0036 <dimonchik0036@gmail.com>
2017-08-06 00:35:53 +0700

    Update Users struct

--- core/users.go
+++ core/users.go
@@ -1,12 +1,20 @@
 package core
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 )
 
 const (
 	PlatformTg = "tg"
 	PlatformVk = "vk"
+)
+
+const (
+	UserLayout = "2006/01/02 15-04-05"
 )
 
 type User struct {
@@ -19,71 +27,80 @@ type User struct {
 	DateCreated         int64      `json:"date_created"`
 	DateLastActivities  int64      `json:"date_last_activities"`
 	ContinuationCommand bool       `json:"command_in_queue"`
-	CurrentCommand      Command    `json:"command"`
+	CurrentCommand      *Command   `json:"command"`
 	QueueMux            sync.Mutex `json:"-"`
-	Queue               []Command  `json:"queue"`
+	Queue               []*Command `json:"queue"`
+}
+
+func (u *User) String() string {
+	return strconv.FormatInt(u.ID, 10) + ", " + u.Username + ", " + u.FirstName + ", " + u.LastName
+}
+
+func (u *User) NewUserString(usernamePrefix string) string {
+	return fmt.Sprintf("ID:%d\n"+
+		"Ник: %s\n"+
+		"Имя: %s\n"+
+		"Фамилия: %s\n"+
+		"Дата регистрации: %s", u.ID, usernamePrefix+u.Username, u.FirstName, u.LastName, time.Unix(u.DateCreated, 0).Format(UserLayout))
+}
+
+func key(prefix string, id int64) string {
+	return prefix + strconv.FormatInt(id, 10)
 }
 
 type Users struct {
-	Mux   sync.RWMutex    `json:"-"`
-	Users map[int64]*User `json:"users"`
+	Mux   sync.RWMutex     `json:"-"`
+	Users map[string]*User `json:"users"`
 }
 
-func (u *Users) SetUser(user *User) {
-	u.Mux.Lock()
-	defer u.Mux.Unlock()
-	u.Users[user.ID] = user
-}
-
-func (u *Users) User(id int64) *User {
+func (u *Users) User(prefix string, id int64) *User {
 	u.Mux.RLock()
 	defer u.Mux.RUnlock()
-	return u.Users[id]
+	return u.Users[key(prefix, id)]
 }
 
-//type Key struct {
-//	Platform string `json:"platform"`
-//	ID       int64  `json:"id"`
-//}
-//
-//type Users struct {
-//	Mux   sync.RWMutex  `json:"-"`
-//	Users map[Key]*User `json:"users"`
-//}
-//
-//func (u *Users) SetUser(prefix string, user *User) {
-//	u.Mux.Lock()
-//	u.Users[Key{
-//		Platform: prefix,
-//		ID:       user.ID,
-//	}] = user
-//	u.Mux.Unlock()
-//}
-//
-//func (u *Users) SetVkUser(user *User) {
-//	u.SetUser(PlatformVk, user)
-//}
-//
-//func (u *Users) SetTgUser(user *User) {
-//	u.SetUser(PlatformTg, user)
-//}
-//
-//func (u *Users) PlatformUsers(platform string) (result []*User) {
-//	u.Mux.RLock()
-//	defer u.Mux.RUnlock()
-//	for key, u := range u.Users {
-//		if key.Platform == platform {
-//			result = append(result, u)
-//		}
-//	}
-//
-//	return
-//}
-//
-//func (u *Users) TgUsers() []*User {
-//	return u.PlatformUsers(PlatformTg)
-//}
-//
-//func (u *Users) VkUsers() []*User {
-//	return u.PlatformUsers(PlatformVk)
-//}
+func (u *Users) VkUser(id int64) *User {
+	return u.User(PlatformVk, id)
+}
+
+func (u *Users) TgUser(id int64) *User {
+	return u.User(PlatformTg, id)
+}
+
+func (u *Users) SetUser(prefix string, user *User) {
+	u.Mux.Lock()
+	defer u.Mux.Unlock()
+	if u.Users == nil {
+		u.Users = map[string]*User{}
+	}
+
+	u.Users[key(prefix, user.ID)] = user
+}
+
+func (u *Users) SetVkUser(user *User) {
+	u.SetUser(PlatformVk, user)
+}
+
+func (u *Users) SetTgUser(user *User) {
+	u.SetUser(PlatformTg, user)
+}
+
+func (u *Users) PlatformUsers(platform string) (result []*User) {
+	u.Mux.RLock()
+	defer u.Mux.RUnlock()
+	for key, u := range u.Users {
+		if strings.Contains(key, platform) {
+			result = append(result, u)
+		}
+	}
+
+	return
+}
+
+func (u *Users) TgUsers() []*User {
+	return u.PlatformUsers(PlatformTg)
+}
+
+func (u *Users) VkUsers() []*User {
+	return u.PlatformUsers(PlatformVk)
+}
